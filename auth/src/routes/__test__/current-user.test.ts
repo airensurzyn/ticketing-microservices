3df06b7177bbbfa4b57ad528fef11029dacafd40
Airen <nyzrus@gmail.com>
2020-10-29 18:42:35 -0700

    Write jest tests for auth routes

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,26 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('responds with details about the current user', async () => {
+    const cookie = await global.signup();
+
+    const response = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+
+    expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null when user is not signed in', async () => {
+
+    const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+
+    expect(response.body.currentUser).toEqual(null);
+
+});
+
