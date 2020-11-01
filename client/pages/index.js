29510d340f7e62a796442437146cad76fe457ea0
Airen <nyzrus@gmail.com>
2020-10-31 20:15:30 -0700

    Frontend signin and signup

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,22 @@
-export default () => {
-	return <h1>Landing Page</h1>;
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+	return currentUser ? (
+		<h1>You are signed in</h1>
+	) : (
+		<h1>You are not signed in</h1>
+	);
 };
+
+LandingPage.getInitialProps = async (context) => {
+	const client = buildClient(context);
+
+	const { data } = await client.get('/api/users/currentuser');
+
+	return data;
+};
+
+export default LandingPage;
+
+// http://__service__.__namespace__.svc.clster.local
+// http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
