bf2d4b5e328ee6328a75d3672160d24867f153cb
Airen <nyzrus@gmail.com>
2020-11-29 16:56:02 -0800

    Add order expiration service

---
+++ expiration/src/nats-wrapper.ts
@@ -0,0 +1,33 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class NatsWrapper {
+  private _client?: Stan;
+
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot access NATS client before connecting');
+    }
+
+    return this._client;
+  }
+
+  connect(clusterId: string, clientId: string, url: string) {
+    console.log(url);
+    console.log(clusterId);
+    console.log(clientId);
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise<void>((resolve, reject) => {
+      this.client.on('connect', () => {
+        console.log('Connected to NATS');
+        resolve();
+      });
+      this.client.on('error', (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
+// Note the above line is exporting an instance of the class defined in this file not the class itself. This is how you secure a Singleton
---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,20 @@
+import { ExpirationCompleteEvent, Listener, OrderStatus, Subjects } from '@airentickets/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+        const order = await Order.findById(data.orderId);
+
+        if(!order) {
+            throw new Error('Order not found');
+        }
+
+        order.set({status: OrderStatus.Cancelled});
+        await order.save();
+    }
+}
